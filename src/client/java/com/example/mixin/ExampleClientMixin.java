1d46fa272957556133691feb7089e882d23b6b88
kasgsc8g326grrty92y9rOI1ED891SDbOT <122430173+kasgsc8g326grrty92y9rOI1ED891SDbOT@users.noreply.github.com>
2026-02-09 22:29:35 +0800

    Create ExampleClientMixin.java

---
+++ src/client/java/com/example/mixin/ExampleClientMixin.java
@@ -0,0 +1,21 @@
+package com.example.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Environment(EnvType.CLIENT)
+@Mixin(ClientPlayerEntity.class)
+public class ExampleClientMixin {
+    
+    // Make the cooldown indicator always show as ready
+    @Inject(method = "getAttackCooldownProgress", at = @At("HEAD"), cancellable = true)
+    private void hideAttackCooldown(float baseTime, CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(1.0f); // Always show as fully charged
+    }
+}
